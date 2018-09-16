fd70ac0d11b1cdb1056d13b8de658af400de6fab
William <william.ma9215@gmail.com>
2018-09-16 00:13:25 -0400

    Final Version Hopefully

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var parser = require('body-parser');
 
+var port = process.env.PORT || 8080;
+
 var app = express();
 
 app.use(parser.urlencoded({ extended: true }));
@@ -10,6 +12,6 @@ app.use(parser.json());
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
-app.listen(8080, function() {
+app.listen(port, function() {
   console.log("App listening on port 8080 . . .");
 });
